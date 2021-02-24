611de3f30ee76df0efdef2ce13d41de5ee9e89eb
Dorian09 <54755064+Dorian09@users.noreply.github.com>
2021-02-24 00:11:01 -0500

    final2

---
+++ resources/views/template/encabezado.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>SAN JOSE ST.BONAVENTURE HOSPITAL</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{!! asset('estilo/img/favicon.png') !!}" rel="icon">
+  <link href="{!! asset('estilo/img/apple-touch-icon.png') !!}" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{!! asset('estilo/vendor/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet">
+  <link href="{!! asset('estilo/vendor/bootstrap-icons/bootstrap-icons.css') !!}" rel="stylesheet">
+  <link href="{!! asset('estilo/vendor/boxicons/css/boxicons.min.css') !!}" rel="stylesheet">
+  <link href="{!! asset('estilo/vendor/glightbox/css/glightbox.min.css') !!}" rel="stylesheet">
+  <link href="{!! asset('estilo/vendor/remixicon/remixicon.css') !!}" rel="stylesheet">
+  <link href="{!! asset('estilo/vendor/swiper/swiper-bundle.min.css') !!}" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{!! asset('estilo/css/style.css') !!}" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: MeFamily - v4.0.1
+  * Template URL: https://bootstrapmade.com/family-multipurpose-html-bootstrap-template-free/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
---
+++ resources/views/template/menu.blade.php
@@ -0,0 +1,20 @@
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <h1 class="logo"><a href="index.html">SAN JOSE ST.BONAVENTURE</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="active" href="{{url('/')}}">Home</a></li>
+          <li><a href="{{url('/create')}}">Create</a></li>
+          <li><a href="events.html">Events</a></li>
+          <li><a href="gallery.html">Gallery</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header>
